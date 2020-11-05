03f1485d58350f884f50bc1a3bbfd6142f3d8c5a
Steven Retana <stevenretana98@gmail.com>
2020-11-04 22:57:25 -0600

    Header un poco actualizado

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom'
 import './App.css';
 import './Componentes/General/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Componentes/General/Header';
-import Login from './Componentes/Login/Login'
+import Login from './Componentes/Login/Login';
+import CrearGrupo from './Componentes/Paginas/CrearGrupo';
+import CrearRama from './Componentes/Paginas/CrearRama';
+import CrearZona from './Componentes/Paginas/CrearZona';
+import MenuEstructura from './Componentes/Paginas/MenuEstructuras';
+import RegistroMiembro from './Componentes/RegistroMiembro/RegistroMiembro';
+import TrasladarMiembro from './Componentes/RegistroMiembro/TrasladarMiembro';
+import TreeContainer from './Componentes/Tree/TreeContainer';
+import CambiarNombreGrupo from './Componentes/CambiarNombreGrupo/CambiarNombreGrupo';
+import MiembroXElemento from './Componentes/Consultas/miembroXelemento';
 
-function App() {
-  return (
-    
-    <div className="App">
-      <div>    
-        <Header></Header> 
-        <Login></Login>
+class App extends Component{
+//function App() {
+  render(){
+    return (
+    <Router>
+      <div className="App">
+        <Switch>    
+          <Route exact path="/" component={Login}/>;
+          <Route path="login" component={Login}/> 
+          <Route path="registroMiembro" component={RegistroMiembro}/> 
+          <Route path="trasladoMiembro" component={TrasladarMiembro}/> 
+          <Route path="creacionZona" component={CrearZona}/> 
+          <Route path="creacionRama" component={CrearRama}/> 
+          <Route path="creacionGrupo" component={CrearGrupo}/> 
+          <Route path="estructura" component={MenuEstructura}/> 
+          <Route path="edicionGrupo" component={CambiarNombreGrupo}/> 
+          <Route path="consultaMiembrosPorElemento" component={MiembroXElemento}/> 
+          <Route path="arbolEstructural" component={TreeContainer}/> 
+        </Switch>
       </div>
-
-
-
-    </div>
-  );
+    </Router>
+    );
+  }
 }
-
 export default App;
--- FrontEnd/my-app/src/Componentes/General/Header.js
+++ FrontEnd/my-app/src/Componentes/General/Header.js
@@ -5,20 +5,71 @@ import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
 import FormControl from 'react-bootstrap/FormControl'
 import Image from 'react-bootstrap/Image'
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router';
+import axios from 'axios';
 import './Header.css'
 
 
 class Header extends Component {
     state = {
-
+        isAuth: true,
     }
 
+    componentDidMount(){
+        {/*if(this.props.isStore) this.state.isStore=true;          
+         var self=this;
+         axios.get('/showSession').then(function(res){
+             if(res.data.loggedIn == true) self.setState({isAuth:true})
+             else self.setState({isAuth:false});
+         })
+        */}
+    }
+
+    logOut(){
+        try {this.props.reload()} catch(error){}
+        axios.get("logOut",{})
+        .then(function (res) {
+          })
+          .catch(function (err) {
+          });        
+        this.setState({
+            isAuth:false
+        })
+    }
 
     render() {
-        return (
-            <Navbar className="header" bg="dark" variant="dark">
-                <Navbar.Brand href="/" className="textito">Movilize!!</Navbar.Brand>
-            </Navbar>
+        var session = this.state.isAuth;
+        return (<>
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            </head>
+            <header className="header">
+                <div className="topContainer">
+                    {/* Home logo */}
+                    <h1 display="inline-block">
+                        <Link className="link" to="/">Movilize!!</Link>
+                    </h1>
+                    <div id="menu">
+                        <Route render={() => {
+                            if(!session){ return <>{" | "} <Link className="link" to="/login">Iniciar Sesión</Link></>}
+                            else{ return <></> }
+                        }}/>
+                        <Route render={() => {
+                            if(!session){ return <>{" | "} <Link className="link" to={{pathname:"/registroMiembro", state:{ newAdmin: false }}}>Registrarse</Link></>}
+                            else{ return <></> }
+                        }}/>
+                        <Route render={() => {
+                            if(session){ return <>{" | "}<Link className="link" to="/" onClick={() => this.logOut()}>Cerrar sesión</Link></>}
+                            else{ return <></> }
+                        }}/>
+                       <Route render={() => {
+                            return <>{" | "} <Link className="link" onClick={()=>this.showSession()}>Show Session</Link></>
+                        }}/>
+                    </div>
+                </div>
+            </header>
+            </>
         )
 
     }
